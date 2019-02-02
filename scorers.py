c5613866ea5bb61f8c4eee8e4b584fa74226e91f
Ján Hrnko <jan.hrnko@nexthop.sk>
2019-02-02 17:38:26 +0100

    optimize ngram scoring (calculate log probs from files only once)

--- scorers.py
+++ scorers.py
@@ -2,25 +2,29 @@ from collections import Counter
 from math import log10
 
 class ngram_score(object):
-    def __init__(self,ngramfile,sep=' '):
-        ''' load a file containing ngrams and counts, calculate log probabilities '''
-        self.ngrams = {}
-        with open(ngramfile) as file:
-            for line in file:
-                key,count = line.split(sep) 
-                self.ngrams[key] = int(count)
 
-        self.L = len(key)
-        self.N = sum(self.ngrams.values())
-        #calculate log probabilities
-        for key in self.ngrams.keys():
-            self.ngrams[key] = log10(float(self.ngrams[key])/self.N)
-        self.floor = log10(0.01/self.N)
+    ngrams = {}
+
+    def __init__(self,ngramfile,sep=' '):
+        ''' load a file containing ngrams and counts, calculate log probabilities and keep them in memory '''
+        if not ngram_score.ngrams:
+            with open(ngramfile) as file:
+                for line in file:
+                    key,count = line.split(sep) 
+                    ngram_score.ngrams[key] = int(count)
+
+            self.L = len(key)
+            self.N = sum(ngram_score.ngrams.values())
+            #calculate log probabilities
+            for key in ngram_score.ngrams.keys():
+                ngram_score.ngrams[key] = log10(float(ngram_score.ngrams[key])/self.N)
+            self.floor = log10(0.01/self.N)
 
     def score(self,text):
         ''' compute the score of text (n-gram) '''
         score = 0
         ngrams = self.ngrams.__getitem__
+        
         for i in range(len(text)-self.L+1):
             if text[i:i+self.L] in self.ngrams: score += ngrams(text[i:i+self.L])
             else: score += self.floor
